8d089b9d3faa4452beab3af85273976caa37bb58
FatihMedAmine <fatihmohamedamin@gmail.com>
2024-12-11 18:24:49 +0100

    #JavaClasses

--- backend/src/main/java/com/location/repository/UsersRepository.java
+++ backend/src/main/java/com/location/repository/UsersRepository.java
@@ -4,5 +4,5 @@ import com.location.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends JpaRepository<Users, String> {
+public interface UsersRepository extends JpaRepository<Users, String> {
 }
