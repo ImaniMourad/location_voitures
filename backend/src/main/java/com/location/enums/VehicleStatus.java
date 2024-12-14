1d5fc465fcb9e07c50436bec9fca52fb89c53bb0
FatihMedAmine <fatihmohamedamin@gmail.com>
2024-12-14 12:39:39 +0100

    # best practices architect

--- backend/src/main/java/com/location/enums/VehicleStatus.java
+++ backend/src/main/java/com/location/enums/VehicleStatus.java
@@ -1,6 +1,6 @@
-package com.location.model;
+package com.location.enums;
 
-public enum Status {
+public enum VehicleStatus  {
     AVAILABLE,  // Disponible
     RESERVED,   // Réservé
     IN_MAINTENANCE; // En entretien
